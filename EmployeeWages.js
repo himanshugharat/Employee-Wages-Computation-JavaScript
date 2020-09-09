02dfe57c23cb45c1ec9c544716a1249ae34083f3
himanshu <himanshugharat44@gmail.com>
2020-09-09 04:56:04 -0500

    ADDED: the code to now calculate for part time employee

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,11 +1,14 @@
 const EMP_RATE_PER_HOUR = 20;
 let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
 let empWages = 0;
 let empHrs = 0;
-let presentCheck = parseInt(Math.random() * 10) % 2;
+let presentCheck = parseInt(Math.random() * 10) % 3;
 if (presentCheck == IS_FULL_TIME)
     empHrs = 8;
+if (presentCheck == IS_PART_TIME)
+    empHrs = 4;
 else
     empHrs = 0
 empWages = empHrs * EMP_RATE_PER_HOUR;
-console.log("EMployee daily wages are "+empWages);
+console.log("EMployee daily wages are " + empWages);
