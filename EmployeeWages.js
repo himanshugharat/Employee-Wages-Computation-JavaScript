abaa4a891f98294a86a10fae42b89ac2fb7bf00c
himanshu <himanshugharat44@gmail.com>
2020-09-09 09:20:42 -0500

    ADDED: the code to get daily wages and monthly wages at same time

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -7,28 +7,28 @@ let totalEmpWages = 0;
 let totalEmpHrs = 0;
 let totalWages = 0;
 let totalWorkingDays = 0
-function WageCalculator(){
-while (totalEmpHrs < MAX_HOURS_A_MONTH && totalWorkingDays < MIN_NUM_OF_DAYS) {
-    let empHrs = 0;
-    let empWages = 0;
-    let presentCheck = parseInt(Math.random() * 10) % 3;
-    totalWorkingDays++;
-    switch (presentCheck) {
-        case IS_FULL_TIME:
-            empHrs = 8;
-            break;
-        case IS_PART_TIME:
-            empHrs = 4;
-            break;
-        default:
-            empHrs = 0;
-            break;
+function WageCalculator() {
+    while (totalEmpHrs < MAX_HOURS_A_MONTH && totalWorkingDays < MIN_NUM_OF_DAYS) {
+        let empHrs = 0;
+        let empWages = 0;
+        let presentCheck = parseInt(Math.random() * 10) % 3;
+        totalWorkingDays++;
+        switch (presentCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
+        empWages = empHrs * EMP_RATE_PER_HOUR;
+        totalWages += empWages;
+        console.log("Employee daily wages are " + empWages + " and till date total wages are " + totalWages);
     }
-    empWages = empHrs * EMP_RATE_PER_HOUR;
-    totalWages += empWages;
-    console.log("EMployee daily wages are " + empWages);
-}
-console.log("total monthly wages" + totalWages);
-console.log("total working days" + totalWorkingDays);
+    console.log("total monthly wages" + totalWages);
+    console.log("total working days" + totalWorkingDays);
 }
 WageCalculator();
