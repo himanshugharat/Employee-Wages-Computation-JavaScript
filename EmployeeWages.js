6320d081b718bc7d617ca1187f2ca7d37d52d953
himanshu <himanshugharat44@gmail.com>
2020-09-09 05:10:17 -0500

    ADDED: code to calculate the monthly wages of employee

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,19 +1,26 @@
 const EMP_RATE_PER_HOUR = 20;
-let IS_FULL_TIME = 1;
-let IS_PART_TIME = 2;
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const MIN_NUM_OF_DAYS = 20;
 let empWages = 0;
 let empHrs = 0;
+let totalWages = 0;
 let presentCheck = parseInt(Math.random() * 10) % 3;
-switch (presentCheck) {
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    default:
-        empHrs = 0;
-        break;
+for (let days = 0; days < MIN_NUM_OF_DAYS; days++) {
+    switch (presentCheck) {
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        default:
+            empHrs = 0;
+            break;
+    }
+    empWages = empHrs * EMP_RATE_PER_HOUR;
+    totalWages += empWages;
+    console.log("EMployee daily wages are " + empWages);
 }
-empWages = empHrs * EMP_RATE_PER_HOUR;
-console.log("EMployee daily wages are " + empWages);
+console.log("total monthly wages" + totalWages)
+}
