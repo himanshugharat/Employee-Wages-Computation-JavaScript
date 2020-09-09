016ab978997fcfb3ab0faa7fd75f153fb82b6df7
himanshu <himanshugharat44@gmail.com>
2020-09-09 04:39:23 -0500

    ADDED: the code to calculate their daily wages

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,7 +1,13 @@
+const WAGES_PER_HOUR=20;
+const FULL_DAY_HOUR=8;
+let dailyWages=0;
 let present = parseInt(Math.random() * 10 % 2);
 if (present == 1) {
-    console.log("present");
+    console.log("Employee is present");
+    dailyWages=WAGES_PER_HOUR*FULL_DAY_HOUR;
 }
 else {
-    console.log("absent");
+    console.log("Employee is absent");
+    dailyWages=0;
 }
+console.log(dailyWages);
